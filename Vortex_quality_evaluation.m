481b0dae1139b2e23ef6e3723030b32d369a30f8
Mateusz Szatkowski <53095764+corlic29@users.noreply.github.com>
2022-01-20 09:44:13 -0500

    Add files via upload

--- Vortex_quality_evaluation.m
+++ Vortex_quality_evaluation.m
@@ -10,8 +10,27 @@ Authors: Mateusz Szatkowski, Brandon Norton, Rosario Porras - Aguilar
 Contact: mateusz.szatkowski@pwr.edu.pl
 %}
 
+%{
+Script additionally uses functions:
+
+intersections
+Douglas Schwarz (2022). Fast and Robust Curve Intersections (https://www.mathworks.com/matlabcentral/fileexchange/11837-fast-and-robust-curve-intersections), MATLAB Central File Exchange. Retrieved January 20, 2022.
+
+&
+rotateAround
+Jan Motl (2022). Rotate an image around a point (https://www.mathworks.com/matlabcentral/fileexchange/40469-rotate-an-image-around-a-point), MATLAB Central File Exchange. Retrieved January 20, 2022.
+%}
+
+%{
+The desired doughnut_ratio value can be described by
+
+desired_doughnut_ratio=1.608*abs(m)^0.5102-0.7913
+where m denotes the input vortex topological charge
+
+%}
 %% Load image
-[FileName,PathName] = uigetfile({'*.jpg'; '*.bmp';}, '.bmp','Select file','Multiselect','off');
+[FileName,PathName] = uigetfile({'*.png';'*.jpg'; '*.bmp'; }, '.bmp','Select file','Multiselect','off');
+
 
 I=im2double(imread([PathName FileName]));
 %I=imadjust(I);
@@ -38,17 +57,27 @@ imwrite(I,[PathName strjoin(FileName(z)) '_adjusted.bmp'])
 
 
 %% Quality inspection
+%Eccentricity
 [LG_parameters] = Get_LG_parameters_manual_2(I,vortex_point_x, vortex_point_y);
 eccentricity=LG_parameters(1)
 
-[peaks_parameters] = Get_peaks_parameters_manual_2(I,vortex_point_x, vortex_point_y);
-peak_to_valley=peaks_parameters(1)
-peaks_difference=peaks_parameters(2)
-proportions=peaks_parameters(3)
+%x and y axes
+[peaks_parameters1] = Get_peaks_parameters_manual_2(I,vortex_point_x, vortex_point_y);
+
+%Diagonal axes
+I=rotateAround(I,vortex_point_y,vortex_point_x,45);
+[peaks_parameters2] = Get_peaks_parameters_manual_2(I,vortex_point_x, vortex_point_y);
+
+%Mean values
+peak_to_valley=mean([peaks_parameters1(1),peaks_parameters2(1)],'all')
+peaks_difference=mean([peaks_parameters1(2),peaks_parameters2(2)],'all')
+doughnut_ratio=mean([peaks_parameters1(3),peaks_parameters2(3)],'all')
 
 close all
 
-results=[LG_parameters, peaks_parameters];
+%% Results
+results=[LG_parameters, peak_to_valley, peaks_difference, doughnut_ratio];
+
 
 
 
